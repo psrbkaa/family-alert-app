7ad6728e6d51ab6bce9e7ff482d242bdc88c6b19
psrbkaa <044.rakapsrb@gmail.com>
2025-04-09 17:31:36 +0700

    first init github

--- frontend/service-worker.js
+++ frontend/service-worker.js
@@ -1,41 +1,24 @@
-const CACHE_NAME = 'family-alert-v1';
-const urlsToCache = [
-  '/',
-  '/frontend/index.html',
-  '/frontend/style.css',
-  '/manifest.json',
-  '/icons/icon-192x192.png',
-  '/icons/icon-512x512.png'
-];
-
 self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(urlsToCache);
-    })
-  );
-  console.log('[Service Worker] Installed');
-});
-
-self.addEventListener('activate', (event) => {
-  console.log('[Service Worker] Activated');
-  event.waitUntil(
-    caches.keys().then((cacheNames) =>
-      Promise.all(
-        cacheNames.map((name) => {
-          if (name !== CACHE_NAME) {
-            return caches.delete(name);
-          }
-        })
-      )
-    )
-  );
-});
-
-self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
-  );
-});
+    event.waitUntil(
+      caches.open('family-alert-cache').then((cache) => {
+        const filesToCache = [
+          '/',
+          '/index.html',
+          '/login.html',
+          '/register.html',
+          '/styles/login.css',
+          '/scripts/socket.js',
+          '/manifest.json',
+          '/service-worker.js',
+          // Daftar file lainnya
+        ];
+  
+        console.log('File yang akan dicache:', filesToCache);
+  
+        return cache.addAll(filesToCache).catch((err) => {
+          console.error('Gagal menambahkan file ke cache:', err);
+        });
+      })
+    );
+  });
+  
